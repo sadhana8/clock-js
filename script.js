1fa3c84d8afe74d670fc64ea746f0882ef850c3a
sadhanasudhag@gmail.com <sadhanasudhag@gmail.com>
2024-03-16 21:10:57 +0545

    clock project using javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+setInterval(()=>{
+let currentDate = new Date();
+
+hrs.innerHTML = (currentDate.getHours()<10?"0":"") + currentDate.getHours();
+min.innerHTML = (currentDate.getMinutes()<10?"0":"") + currentDate.getMinutes();
+sec.innerHTML = (currentDate.getSeconds()<10?"0":"") + currentDate.getSeconds();
+},1000)
